f93765c08c47962ccf4bb0ffe2ea23988d28fa37
SualaDev <81395134+SualaDev@users.noreply.github.com>
2023-02-24 15:55:13 +0100

    added routes

--- app-frontend/src/router/routes.js
+++ app-frontend/src/router/routes.js
@@ -4,7 +4,8 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
+      { path: '', component: () => import('pages/LogIn.vue') },
+      { path: '/signup', component: () => import('pages/SignUp.vue') }
     ]
   },
 
