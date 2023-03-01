8f27de637b175475cd6ee9ea1c1ee15651fe1e2c
SualaDev <81395134+SualaDev@users.noreply.github.com>
2023-03-01 15:15:16 +0100

    hame layout created

---
+++ app-frontend/src/layouts/HomeLayout.vue
@@ -0,0 +1,124 @@
+<template>
+  <q-layout class="page-container" view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          Quasar App
+        </q-toolbar-title>
+
+        <div>CRUD Application By Suala Amotsuka</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      <q-img class="header-image" src="~src/static/mountain.jpg">
+        <div  class="absolute-bottom bg-transparent text-wrapper">
+          <p>Suala Amotsuka</p>
+          <p>@SualaDev</p>
+        </div>
+      </q-img>
+      <div class="list-section">
+        <q-btn to="/cart" class="list-item">
+          <q-icon name="shopping_cart"></q-icon>
+          <p>Cart</p>
+        </q-btn>
+      </div>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+const linksList = [
+  {
+    title: 'Docs',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'Github',
+    caption: 'github.com/quasarframework',
+    icon: 'code',
+    link: 'https://github.com/quasarframework'
+  },
+  {
+    title: 'Discord Chat Channel',
+    caption: 'chat.quasar.dev',
+    icon: 'chat',
+    link: 'https://chat.quasar.dev'
+  },
+  {
+    title: 'Forum',
+    caption: 'forum.quasar.dev',
+    icon: 'record_voice_over',
+    link: 'https://forum.quasar.dev'
+  },
+  {
+    title: 'Twitter',
+    caption: '@quasarframework',
+    icon: 'rss_feed',
+    link: 'https://twitter.quasar.dev'
+  },
+  {
+    title: 'Facebook',
+    caption: '@QuasarFramework',
+    icon: 'public',
+    link: 'https://facebook.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  }
+]
+export default defineComponent({
+  name: 'MainLayout',
+  // components: {
+  //   EssentialLink
+  // },
+  setup () {
+    const leftDrawerOpen = ref(false)
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
+<style lang="scss">
+.header-image img{
+  width: 100%;
+}
+.list-section{
+  margin: 0;
+  padding: 0;
+}
+.list-item{
+  display: flex;
+  column-gap: 2rem;
+  padding: 1.5rem;
+  align-items: center;
+}
+</style>
