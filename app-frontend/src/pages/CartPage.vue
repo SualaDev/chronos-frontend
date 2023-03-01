167cce273bba0154eddac9b8fdb13fa91ed04ead
SualaDev <81395134+SualaDev@users.noreply.github.com>
2023-03-01 15:15:44 +0100

    cart page and home page created

---
+++ app-frontend/src/pages/CartPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="main-container">
+    <div class="sub-container">
+      <h2>Shopping Cart</h2>
+      <p>Remove All</p>
+      <h2>Apple Juice </h2>
+      <button>+</button>
+      <p>2</p>
+      <button>-</button>
+      <p>$2.99</p>
+      <p>Save for later</p>
+      <p>Remove</p>
+      <h2>Sub-Total</h2>
+      <p>2 items</p>
+      <h3>$6.18</h3>
+      <button>Checkout</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:"CartPage"
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ app-frontend/src/pages/HomePage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Welcome to Your Store
+  </div>
+</template>
+
+<script>
+  export default {
+    name:"HomePage"
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
